5533e70ddababac2bcccb03b0672ed86f7176b2f
Helcio da Silva <helciodasilva@gmail.com>
2018-04-22 11:40:14 -0300

    Initial commit

---
+++ src/main/java/com/helciodasilva/flying/saucer/samples/ApplicationStarter.java
@@ -0,0 +1,12 @@
+package com.helciodasilva.flying.saucer.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStarter.class, args);
+	}
+}
